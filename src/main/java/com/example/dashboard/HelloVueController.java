28ad7211cf2ec87d2056cd1405120ff6792a4379
22050087 <22050087@myrp.edu.sg>
2023-07-16 21:39:02 +0800

    Create Hello page

---
+++ src/main/java/com/example/dashboard/HelloVueController.java
@@ -0,0 +1,20 @@
+package com.example.dashboard;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloVueController {
+  @GetMapping("/")
+    ModelAndView showHelloPage() {
+        Map<String, Object> model = new HashMap<>();
+        model.put("title", "Hello Vue!");
+        model.put("chartData", Arrays.asList(7,6,5,4,3,2,1));
+        return new ModelAndView("hello-vue.html", model);
+    }
+}
